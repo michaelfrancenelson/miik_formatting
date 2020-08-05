a75303bc8ad623db7b268e8956fd351319c1de92
Michael France Nelson <michaelfrancenelson@gmail.com>
2020-08-05 09:53:28 -0400

    adding some more style docs

---
+++ functions/rmd_functions.R
@@ -0,0 +1,19 @@
+require(here)
+
+
+
+colorize <- function(x, color) {
+  if (knitr::is_latex_output()) {
+    sprintf("\\textcolor{%s}{%s}", color, x)
+  } else if (knitr::is_html_output()) {
+    sprintf("<span style='color: %s;'>%s</span>", color, x)
+  } else x
+}
+
+bold_col <- function(x, color) {
+  if (knitr::is_latex_output()) {
+    sprintf("\\textbf{\\textcolor{%s}{%s}}", color, x)
+  } else if (knitr::is_html_output()) {
+    sprintf("<span style='color: %s;'>%s</span>", color, x)
+  } else x
+}

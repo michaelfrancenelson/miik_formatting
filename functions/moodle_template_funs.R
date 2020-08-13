bf936055986e79fa7fc6b24d395b1b609b272229
Michael France Nelson <michaelfrancenelson@gmail.com>
2020-08-13 00:10:44 -0400

    clickable expanding image and lightbox generating functions now working

--- functions/moodle_template_funs.R
+++ functions/moodle_template_funs.R
@@ -1,192 +1,91 @@
 
 
-
-
-expandable_html_image = function(filename, click_message = "[Click to expand image]")
-{
-
-if (FALSE)
-{
-  filename = "github_desktop_sign_in.PNG"
-  click_message = "[Click to expand image]"
-  thumb_width = NULL
-  thumb_width = "100px"
-  img_ref = NULL
-  lb_width = "80vw"
-  img_width = "70%"
-}
-
-href_image = paste0("img", sample(9999999, 1))
-href_close = paste0(sample(letters, 12), collapse = "")
-rmd_filename = sprintf(fmt = '![](%s)', filename)
-
-
-fmt = paste(
-  '<a href="#%4$s"></a>',
-  '<a class="thumb" href="#%3$s">',
-  '  <figure>',
-  '    %2$s',
-  '    <figcaption>%1$s</figcaption>',
-  '  </figure>',
-  '</a>',
-  '<div class="lb" id="%3$s">',
-  '  %2$s',
-  '  <a class="lb-close" href="#%4$s"></a>',
-  '</div>', 
-  sep = "\n")
-
-# format text numberings:
-# 1  click message
-# 2  image file name, formatted for markdown image inclusion
-# 3  href code for image
-# 4  href code for close
-
-
-
-(out =
-  sprintf(
-  fmt = fmt,
-  click_message, rmd_filename, href_image, href_close))
-
-cat(out)
-invisible(out)
-}
-
-
-
-
-if (FALSE)
-{
-
-expandable_html_image = function(filename, click_message = "[Click to expand image]", thumb_width = NULL, img_width = NULL, lb_width = NULL, img_ref = NULL)
+expandable_html_image = function(filename, click_message = "[Click to expand image]", img_width = NULL)
 {
   
   if (FALSE)
   {
     filename = "github_desktop_sign_in.PNG"
     click_message = "[Click to expand image]"
-    thumb_width = NULL
-    thumb_width = "100px"
-    img_ref = NULL
-    lb_width = "80vw"
-    img_width = "70%"
+    img_width = 70
+    expandable_html_image(filename, click_message, img_width)
   }
   
-  href = ifelse(
-    is.null(img_ref),
-    paste0("img", sample(9999999, 1)),
-    img_ref)
+  href_image = paste0("img", sample(9999999, 1))
+  href_close = paste0(sample(letters, 12), collapse = "")
+  rmd_filename = sprintf(fmt = '![](%s)', filename)
   
-  href_1 = 
-    paste0(sample(letters, 12), collapse = "")
-  
-  img_path = list.files(path = here::here(), pattern = filename, recursive = TRUE, full.names = TRUE)[1]
-  
-  thumb_style =
-    ifelse(
-      is.null(thumb_width),
-      "",
-      sprintf('style="width: %s;"', thumb_width)
-    )
-  
-  lb_style = 
-    ifelse(
-      is.null(lb_width),
-      "",
-      sprintf('style="width: %s;"', lb_width)
-    )
-  
-  
-  thumb_style_md =
-    ifelse(
-      is.null(thumb_width),
-      "",
-      sprintf('{width=%s}', thumb_width)
-    )
-  
-  img_style_md = 
-    ifelse(
-      is.null(img_width),
-      "",
-      sprintf('{width=%s}', img_width)
-    )
-  
-  
-  img_thumb_md = sprintf(fmt = '![](%s)%s', img_path, thumb_style_md)
-  
-  img_thumb_md = sprintf(fmt = '![](%s)%s', filename, thumb_style_md)
-  img_lb_md = sprintf(fmt = '![](%s)', filename)
-  
-  img_lb_md = sprintf(fmt = '![](%s)%s', filename, img_style_md)
-  
-  
-  '50% - var(--img-width) * 0.5'
+  lb_class_name = ifelse(
+    is.null(img_width),
+    "lb",
+    paste0("lb", img_width)
+  )
   
   fmt = paste(
-    
-    '<a href="#%6$s"></a>',
-    '<a class="thumb" href="#%2$s">',
+    '<a href="#%4$s"></a>',
+    '<a class="thumb" href="#%3$s">',
     '  <figure>',
-    # '    <img %4$s src="@@PLUGINFILE@@/%9$s"/>',
-    # '    <img %4$s src="%3$s"/>',
-    '    %7$s',
+    '    %2$s',
     '    <figcaption>%1$s</figcaption>',
     '  </figure>',
     '</a>',
-    '<div class="lb" %5$s id="%2$s">',
-    # '  <img src="%3$s"/>',
-    # '  <img src="@@PLUGINFILE@@/%9$s"/>',
-    '  %8$s',
-    '  <a class="lb-close" href="#%6$s"></a>',
-    # '  <a class="lb-close" href="#"></a>',
+    '<div class="%5$s" id="%3$s">',
+    '  %2$s',
+    '  <a class="lb-close" href="#%4$s"></a>',
     '</div>', 
     sep = "\n")
   
-  '@@PLUGINFILE@@'
-  
   # format text numberings:
   # 1  click message
-  # 2  href code
-  # 3  image file path
-  # 4  thumbnail width
-  # 5  lightbox width
-  # 6  test href target for close
-  # 7  markdown thumbnail image path
-  # 8  markdown lightbos image path
-  # 9  image file name (no path)
-  
-  
+  # 2  image file name, formatted for markdown image inclusion
+  # 3  href code for image
+  # 4  href code for close
+  # 5  custom lightbox class name
   
   (out =
       sprintf(
         fmt = fmt,
-        click_message, href, img_path, thumb_style, lb_style, href_1, img_thumb_md, img_lb_md, filename))
+        click_message, rmd_filename, href_image, href_close, lb_class_name ))
+  
+  if (!is.null(img_width))
+    out = paste0(build_lightbox(img_width), out)
   
   cat(out)
   invisible(out)
 }
 
-
-expandable_html_image = function(filename, click_message = "[Click to expand image]", img_ref = NULL)
+build_lightbox = function(img_width)
 {
-  href = ifelse(
-    is.null(img_ref),
-    paste0("img", sample(9999999, 1)),
-    img_ref)
+  fmt_lb = paste(
+    '<style>',
+    ':root {',
+    '  --img-width%1$s: %1$s%2$s;',
+    '}',
+    '.lb%1$s {',
+    'border: var(--lb-border);',
+    'position: fixed;',
+    'height: auto; overflow: auto; text-align: center;',
+    'bottom: 0;',
+    'left: calc(50%2$s - var(--lb-width) * 0.5);',
+    'width: var(--lb-width);',
+    'background: var(--lb-background); opacity: 0;',
+    '}',
+    '.lb%1$s img {',
+    'position: absolute; object-fit: contain;',
+    'right: 0;',
+    'bottom: 0;',
+    'top: 0;',
+    'left: calc(0.5 * (100%2$s - var(--img-width%1$s)));',
+    'width: var(--img-width%1$s); height: auto;',
+    'box-shadow: var(--img-box-shadow);',
+    'border: var(--img-border);',
+    'text-align: center;',
+    '}',
+    '.lb%1$s:target { opacity: 1; top: 0; bottom: 0; }',
+    '.lb%1$s:target img { max-height: 100%2$s; max-width: 100%2$s; }',
+    '.lb%1$s:target .lightbox-close { top: 0; }',
+    '</style>',
+    sep = "\n")
   
-  out = sprintf(fmt = 
-                  '%1$s <br>
-<a class="thumb" href="#%2$s">
-![](%3$s)
-</a> 
-<div class="lb" id="%2$s">
-![](%3$s)
-<a class="lb-close" href="#"></a>
-</div>', click_message, href, filename)
-  cat(out)
+  return(sprintf(fmt = fmt_lb, img_width, "%"))
 }
-}
-
-
-
-# expandable_html_image("abc")

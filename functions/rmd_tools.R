72210c9e8bf3ad2b2d7b4807a069cf740110ca92
Michael France Nelson <michaelfrancenelson@gmail.com>
2020-09-14 17:18:33 -0400

    saving

--- functions/rmd_tools.R
+++ functions/rmd_tools.R
@@ -346,3 +346,27 @@ build_doc = function(
       output_file = paste0(output_file_stem, ".pdf"))
   }
 }
+
+proportional_image_crop = function(
+  filename, 
+  height_pct = 1, 
+  width_pct = 1, 
+  height_offset = 0, 
+  width_offset = 0)
+{
+  require(magick)
+  img = image_read(find_file(filename))
+  return(
+    image_crop(
+      img,
+      sprintf(
+        fmt = "%1$sx%2$s+%3$s+%4$s", 
+        image_info(img)$width * width_pct,
+        image_info(img)$height * height_pct,
+        image_info(img)$width * width_offset,
+        image_info(img)$height * height_offset
+      )
+    )
+  )
+}
+
